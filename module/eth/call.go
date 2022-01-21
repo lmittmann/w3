842012eedd055d424b4246a9ed73c584bb5d74a9
lmittmann <lmittmann@users.noreply.github.com>
2022-01-21 23:10:18 +0100

    added `eth.CallFunc(...)`

--- module/eth/call.go
+++ module/eth/call.go
@@ -4,10 +4,13 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
+	"github.com/lmittmann/w3/core"
 )
 
+// Call requests the output data of the given message.
 func Call(msg ethereum.CallMsg) *CallFactory {
 	return &CallFactory{msg: msg}
 }
@@ -45,6 +48,62 @@ func (f *CallFactory) HandleResponse(elem rpc.BatchElem) error {
 		return err
 	}
 	*f.returns = []byte(f.result)
+	return nil
+}
+
+// CallFunc requests the returns of Func fn at common.Address to with the given
+// args.
+func CallFunc(fn core.Func, to common.Address, args ...interface{}) *CallFuncFactory {
+	return &CallFuncFactory{fn: fn, to: to, args: args}
+}
+
+type CallFuncFactory struct {
+	// args
+	fn      core.Func
+	to      common.Address
+	args    []interface{}
+	atBlock *big.Int
+
+	// returns
+	result  hexutil.Bytes
+	returns []interface{}
+}
+
+func (f *CallFuncFactory) AtBlock(blockNumber *big.Int) *CallFuncFactory {
+	f.atBlock = blockNumber
+	return f
+}
+
+func (f *CallFuncFactory) Returns(returns ...interface{}) *CallFuncFactory {
+	f.returns = returns
+	return f
+}
+
+func (f *CallFuncFactory) CreateRequest() (rpc.BatchElem, error) {
+	input, err := f.fn.EncodeArgs(f.args...)
+	if err != nil {
+		return rpc.BatchElem{}, err
+	}
+
+	msg := ethereum.CallMsg{
+		To:   &f.to,
+		Data: input,
+	}
+	return rpc.BatchElem{
+		Method: "eth_call",
+		Args:   []interface{}{toCallArg(msg), toBlockNumberArg(f.atBlock)},
+		Result: &f.result,
+	}, nil
+}
+
+func (f *CallFuncFactory) HandleResponse(elem rpc.BatchElem) error {
+	if err := elem.Error; err != nil {
+		return err
+	}
+	output := []byte(f.result)
+	if err := f.fn.DecodeReturns(output, f.returns...); err != nil {
+		return err
+	}
 	return nil
 }
 
