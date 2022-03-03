3194a8c5b270eec1d6f53bcdb5efb09da0578daf
lmittmann <lmittmann@users.noreply.github.com>
2022-03-03 23:06:30 +0100

    module/eth: added test case

---
+++ module/eth/block_number_test.go
@@ -0,0 +1,33 @@
+package eth_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/lmittmann/w3"
+	"github.com/lmittmann/w3/module/eth"
+	"github.com/lmittmann/w3/rpctest"
+)
+
+func TestBlockNumber(t *testing.T) {
+	t.Parallel()
+
+	srv := rpctest.NewFileServer(t, "testdata/block_number.golden")
+	defer srv.Close()
+
+	client := w3.MustDial(srv.URL())
+	defer client.Close()
+
+	var (
+		blockNumber     = new(big.Int)
+		wantBlockNumber = w3.I("0xc0fe")
+	)
+	if err := client.Call(
+		eth.BlockNumber().Returns(blockNumber),
+	); err != nil {
+		t.Fatalf("Request failed: %v", err)
+	}
+	if wantBlockNumber.Cmp(blockNumber) != 0 {
+		t.Fatalf("want %d, got %d", wantBlockNumber, blockNumber)
+	}
+}
