246141b8dcbecc5f549eaca73532199de286bef0
lmittmann <lmittmann@users.noreply.github.com>
2022-09-05 15:03:14 +0200

    module/eth: added tests

--- module/eth/tx_test.go
+++ module/eth/tx_test.go
@@ -14,6 +14,19 @@ import (
 	"github.com/lmittmann/w3/module/eth"
 )
 
+var type2Tx = types.NewTx(&types.DynamicFeeTx{
+	ChainID:   big.NewInt(1),
+	Nonce:     261,
+	GasTipCap: w3.I("30.38 gwei"),
+	GasFeeCap: w3.I("32.38 gwei"),
+	Gas:       47238,
+	To:        w3.APtr("0x491D6b7D6822d5d4BC88a1264E1b47791Fd8E904"),
+	Data:      w3.B("0x095ea7b30000000000000000000000007645eec8bb51862a5aa855c40971b2877dae81afffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
+	V:         w3.I("0x1"),
+	R:         w3.I("0x416470241b7db89c67526881b6fd8e145416b294a35bf4280d3079f6308c2d11"),
+	S:         w3.I("0x2c0af1cc55c22c0bab79ec083801da63253453156356fcd4291f50d0f425a0ee"),
+})
+
 func TestTx(t *testing.T) {
 	tests := []testCase[types.Transaction]{
 		{
@@ -30,20 +43,9 @@ func TestTx(t *testing.T) {
 			}),
 		},
 		{
-			Golden: "get_transaction_by_hash__type2",
-			Call:   eth.Tx(w3.H("0xed382cb554ad10e94921d263a56c670669d6c380bbdacdbf96fed625b7132a1d")),
-			WantRet: *types.NewTx(&types.DynamicFeeTx{
-				ChainID:   big.NewInt(1),
-				Nonce:     261,
-				GasTipCap: w3.I("30.38 gwei"),
-				GasFeeCap: w3.I("32.38 gwei"),
-				Gas:       47238,
-				To:        w3.APtr("0x491D6b7D6822d5d4BC88a1264E1b47791Fd8E904"),
-				Data:      w3.B("0x095ea7b30000000000000000000000007645eec8bb51862a5aa855c40971b2877dae81afffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
-				V:         w3.I("0x1"),
-				R:         w3.I("0x416470241b7db89c67526881b6fd8e145416b294a35bf4280d3079f6308c2d11"),
-				S:         w3.I("0x2c0af1cc55c22c0bab79ec083801da63253453156356fcd4291f50d0f425a0ee"),
-			}),
+			Golden:  "get_transaction_by_hash__type2",
+			Call:    eth.Tx(w3.H("0xed382cb554ad10e94921d263a56c670669d6c380bbdacdbf96fed625b7132a1d")),
+			WantRet: *type2Tx,
 		},
 		{
 			Golden:  "get_transaction_by_hash__0x00",
@@ -58,22 +60,46 @@ func TestTx(t *testing.T) {
 	)
 }
 
+func TestTxByBlockHashAndIndex(t *testing.T) {
+	tests := []testCase[types.Transaction]{
+		{
+			Golden:  "get_transaction_by_block_hash_and_index",
+			Call:    eth.TxByBlockHashAndIndex(w3.H("0xa32d159805750cbe428b799a49b85dcb2300f61d806786f317260e721727d162"), 98),
+			WantRet: *type2Tx,
+		},
+		{
+			Golden:  "get_transaction_by_block_hash_and_index__300",
+			Call:    eth.TxByBlockHashAndIndex(w3.H("0xa32d159805750cbe428b799a49b85dcb2300f61d806786f317260e721727d162"), 300),
+			WantErr: fmt.Errorf("w3: response handling failed: not found"),
+		},
+	}
+
+	runTestCases(t, tests,
+		cmp.AllowUnexported(types.Transaction{}, atomic.Value{}),
+		cmpopts.IgnoreFields(types.Transaction{}, "time"),
+	)
+}
+
+func TestTxByBlockNumberAndIndex(t *testing.T) {
+	tests := []testCase[types.Transaction]{
+		{
+			Golden:  "get_transaction_by_block_number_and_index",
+			Call:    eth.TxByBlockNumberAndIndex(big.NewInt(12965001), 98),
+			WantRet: *type2Tx,
+		},
+	}
+
+	runTestCases(t, tests,
+		cmp.AllowUnexported(types.Transaction{}, atomic.Value{}),
+		cmpopts.IgnoreFields(types.Transaction{}, "time"),
+	)
+}
+
 func TestSendTx(t *testing.T) {
 	tests := []testCase[common.Hash]{
 		{
-			Golden: "send_raw_transaction",
-			Call: eth.SendTx(types.NewTx(&types.DynamicFeeTx{
-				ChainID:   big.NewInt(1),
-				Nonce:     261,
-				GasTipCap: w3.I("30.38 gwei"),
-				GasFeeCap: w3.I("32.38 gwei"),
-				Gas:       47238,
-				To:        w3.APtr("0x491D6b7D6822d5d4BC88a1264E1b47791Fd8E904"),
-				Data:      w3.B("0x095ea7b30000000000000000000000007645eec8bb51862a5aa855c40971b2877dae81afffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"),
-				V:         w3.I("0x1"),
-				R:         w3.I("0x416470241b7db89c67526881b6fd8e145416b294a35bf4280d3079f6308c2d11"),
-				S:         w3.I("0x2c0af1cc55c22c0bab79ec083801da63253453156356fcd4291f50d0f425a0ee"),
-			})),
+			Golden:  "send_raw_transaction",
+			Call:    eth.SendTx(type2Tx),
 			WantRet: w3.H("0xed382cb554ad10e94921d263a56c670669d6c380bbdacdbf96fed625b7132a1d"),
 		},
 	}
