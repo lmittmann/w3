e8b856ed0d3669fb7e9dbf983854e7e1342ba214
lmittmann <lmittmann@users.noreply.github.com>
2023-03-10 22:05:53 +0100

    added testcase

--- event_test.go
+++ event_test.go
@@ -143,6 +143,26 @@ func TestEventDecodeArgs(t *testing.T) {
 				big.NewInt(42),
 			},
 		},
+		{ // https://github.com/lmittmann/w3/issues/15
+			Event: MustNewEvent("NameRegistered(string name, bytes32 indexed label, address indexed owner, uint cost, uint expires)"),
+			Log: &types.Log{
+				Address: A("0x283Af0B28c62C092C9727F1Ee09c02CA627EB7F5"),
+				Topics: []common.Hash{
+					H("0xca6abbe9d7f11422cb6ca7629fbf6fe9efb1c621f71ce8f02b9f2a230097404f"),
+					H("0x4e59ffc7ae105a2b19f7f29b63e9f9c5ac28e27bce744a330804c6a89269cec0"),
+					H("0x000000000000000000000000bd08f39b2523426cc1d6961e2d6a9744b3b432b5"),
+				},
+				Data: B("0x000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000165a7d3a3e48ad0000000000000000000000000000000000000000000000000000000066e4d0a50000000000000000000000000000000000000000000000000000000000000009656c666172657373690000000000000000000000000000000000000000000000"),
+			},
+			Args: []any{new(string), new(common.Hash), new(common.Address), new(big.Int), new(big.Int)},
+			WantArgs: []any{
+				ptr("elfaressi"),
+				ptr(H("0x4e59ffc7ae105a2b19f7f29b63e9f9c5ac28e27bce744a330804c6a89269cec0")),
+				APtr("0xbD08F39B2523426Cc1d6961e2d6A9744B3B432b5"),
+				big.NewInt(6291943382206637),
+				big.NewInt(1726271653),
+			},
+		},
 	}
 
 	for i, test := range tests {
