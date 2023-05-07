190f48fea38e0e79dd2f3340174f177c68be8431
lmittmann <lmittmann@users.noreply.github.com>
2023-05-07 23:25:27 +0200

    added `multiEVMLogger`

---
+++ w3test/multi_tracer.go
@@ -0,0 +1,70 @@
+package w3test
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/vm"
+)
+
+// multiEVMLogger is a wrapper around multiple EVMLogger's.
+type multiEVMLogger []vm.EVMLogger
+
+func newMultiEVMLogger(tracers []vm.EVMLogger) vm.EVMLogger {
+	switch len(tracers) {
+	case 0:
+		return nil
+	case 1:
+		return tracers[0]
+	default:
+		return multiEVMLogger(tracers)
+	}
+}
+
+func (m multiEVMLogger) CaptureTxStart(gasLimit uint64) {
+	for _, tracer := range m {
+		tracer.CaptureTxStart(gasLimit)
+	}
+}
+
+func (m multiEVMLogger) CaptureTxEnd(restGas uint64) {
+	for _, tracer := range m {
+		tracer.CaptureTxEnd(restGas)
+	}
+}
+
+func (m multiEVMLogger) CaptureStart(env *vm.EVM, from common.Address, to common.Address, create bool, input []byte, gas uint64, value *big.Int) {
+	for _, tracer := range m {
+		tracer.CaptureStart(env, from, to, create, input, gas, value)
+	}
+}
+
+func (m multiEVMLogger) CaptureEnd(output []byte, gasUsed uint64, err error) {
+	for _, tracer := range m {
+		tracer.CaptureEnd(output, gasUsed, err)
+	}
+}
+
+func (m multiEVMLogger) CaptureEnter(typ vm.OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
+	for _, tracer := range m {
+		tracer.CaptureEnter(typ, from, to, input, gas, value)
+	}
+}
+
+func (m multiEVMLogger) CaptureExit(output []byte, gasUsed uint64, err error) {
+	for _, tracer := range m {
+		tracer.CaptureExit(output, gasUsed, err)
+	}
+}
+
+func (m multiEVMLogger) CaptureState(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, rData []byte, depth int, err error) {
+	for _, tracer := range m {
+		tracer.CaptureState(pc, op, gas, cost, scope, rData, depth, err)
+	}
+}
+
+func (m multiEVMLogger) CaptureFault(pc uint64, op vm.OpCode, gas, cost uint64, scope *vm.ScopeContext, depth int, err error) {
+	for _, tracer := range m {
+		tracer.CaptureFault(pc, op, gas, cost, scope, depth, err)
+	}
+}
