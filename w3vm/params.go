bb06bc547eefbb5e90a11c860977e17c1e3b3387
lmittmann <lmittmann@users.noreply.github.com>
2023-07-11 20:58:49 +0200

    added `defaultChainConfig`

---
+++ w3vm/params.go
@@ -0,0 +1,36 @@
+package w3vm
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/params"
+)
+
+// defaultChainConfig contains every protocol change introduced for Mainnet.
+var defaultChainConfig = &params.ChainConfig{
+	ChainID:                       big.NewInt(1),
+	HomesteadBlock:                new(big.Int),
+	DAOForkBlock:                  nil,
+	DAOForkSupport:                false,
+	EIP150Block:                   new(big.Int),
+	EIP155Block:                   new(big.Int),
+	EIP158Block:                   new(big.Int),
+	ByzantiumBlock:                new(big.Int),
+	ConstantinopleBlock:           new(big.Int),
+	PetersburgBlock:               new(big.Int),
+	IstanbulBlock:                 new(big.Int),
+	MuirGlacierBlock:              new(big.Int),
+	BerlinBlock:                   new(big.Int),
+	LondonBlock:                   new(big.Int),
+	ArrowGlacierBlock:             new(big.Int),
+	GrayGlacierBlock:              new(big.Int),
+	MergeNetsplitBlock:            nil,
+	ShanghaiTime:                  &uint0,
+	CancunTime:                    nil,
+	PragueTime:                    nil,
+	VerkleTime:                    nil,
+	TerminalTotalDifficulty:       nil,
+	TerminalTotalDifficultyPassed: true,
+	Ethash:                        new(params.EthashConfig),
+	Clique:                        nil,
+}
