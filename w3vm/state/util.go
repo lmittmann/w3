9e6544b5587f454695b1613b4880cffe6b7061b3
lmittmann <lmittmann@users.noreply.github.com>
2023-07-16 16:31:39 +0200

    state: added `noopKeyValueStore`

--- w3vm/state/util.go
+++ w3vm/state/util.go
@@ -5,10 +5,13 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/holiman/uint256"
 	"github.com/lmittmann/w3/internal/module"
 	"github.com/lmittmann/w3/w3types"
 )
+
+var hash0 common.Hash
 
 // ethBalance is like [eth.Balance], but returns the balance as [uint256.Int].
 func ethBalance(addr common.Address, blockNumber *big.Int) w3types.CallerFactory[uint256.Int] {
@@ -47,3 +50,47 @@ func (i *uint256OrHash) UnmarshalText(text []byte) error {
 	(*uint256.Int)(i).SetBytes(buf)
 	return nil
 }
+
+// noopKeyValueStore implements a [ethdb.KeyValueStore] that does nothing.
+type noopKeyValueStore struct {
+	ethdb.KeyValueStore
+}
+
+// ethdb.KeyValueReader methods
+func (noopKeyValueStore) Get(key []byte) ([]byte, error) { panic("not implemented") }
+func (noopKeyValueStore) Has(key []byte) (bool, error)   { panic("not implemented") }
+
+// ethdb.KeyValueWriter methods
+func (noopKeyValueStore) Put(key []byte, value []byte) error { panic("not implemented") }
+func (noopKeyValueStore) Delete(key []byte) error            { panic("not implemented") }
+
+// ethdb.KeyValueStater methods
+func (noopKeyValueStore) Stat(property string) (string, error) { panic("not implemented") }
+
+// ethdb.Batcher methods
+func (noopKeyValueStore) NewBatch() ethdb.Batch                 { return new(noopBatch) }
+func (noopKeyValueStore) NewBatchWithSize(size int) ethdb.Batch { return new(noopBatch) }
+
+// ethdb.Iteratee methods
+func (noopKeyValueStore) NewIterator(prefix []byte, start []byte) ethdb.Iterator {
+	panic("not implemented")
+}
+
+// eth.Compacter methods
+func (noopKeyValueStore) Compact(start []byte, limit []byte) error { panic("not implemented") }
+
+// ethdb.Snapshotter methods
+func (noopKeyValueStore) Snapshot() int { panic("not implemented") }
+
+// io.Closer methods
+func (noopKeyValueStore) Close() error { return nil }
+
+// noopBatch implements a [ethdb.Batch] that does nothing.
+type noopBatch struct {
+	ethdb.Batch
+}
+
+func (noopBatch) ValueSize() int                      { return 0 }
+func (noopBatch) Write() error                        { return nil }
+func (noopBatch) Reset()                              {}
+func (noopBatch) Replay(w ethdb.KeyValueWriter) error { return nil }
