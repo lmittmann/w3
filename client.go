a2acd31a6ec422aa69650ed84452c529adb2e0cc
lmittmann <lmittmann@users.noreply.github.com>
2023-07-17 00:09:07 +0200

    moved package doc

--- client.go
+++ client.go
@@ -1,3 +1,7 @@
+/*
+Package w3 implements a blazing fast and modular Ethereum JSON RPC client with
+first-class ABI support.
+*/
 package w3
 
 import (
--- doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package w3 implements a blazing fast and modular Ethereum JSON RPC client with
-first-class ABI support.
-*/
-package w3
