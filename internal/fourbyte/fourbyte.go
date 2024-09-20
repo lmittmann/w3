7ef41f856221f00a08190a4c0993e4db4debfb94
lmittmann <lmittmann@users.noreply.github.com>
2024-09-20 10:12:20 +0200

    internal/fourbyte: init

---
+++ internal/fourbyte/fourbyte.go
@@ -0,0 +1,13 @@
+//go:generate go run gen.go
+
+package fourbyte
+
+import "github.com/lmittmann/w3"
+
+func Function(sig [4]byte) *w3.Func {
+	return functions[sig]
+}
+
+// func Event(topic0 [32]byte) *w3.Event {
+// 	return events[topic0]
+// }
