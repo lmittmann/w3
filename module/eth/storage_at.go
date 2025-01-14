4611c5ee8cbdb3cffc9be8492fa5eefbcf0cc376
lmittmann <lmittmann@users.noreply.github.com>
2025-01-14 10:32:43 +0000

    module/eth: added number support of storage values in `StorageAt`
    Some nodes return storage values not as hash, but as number. E.g `0x42` instead of `0x0000000000000000000000000000000000000000000000000000000000000042`

--- module/eth/storage_at.go
+++ module/eth/storage_at.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/lmittmann/w3/internal/hexutil"
 	"github.com/lmittmann/w3/internal/module"
 	"github.com/lmittmann/w3/w3types"
 )
@@ -12,8 +13,9 @@ import (
 // given common.Hash slot at the given blockNumber. If block number is nil, the
 // slot at the latest known block is requested.
 func StorageAt(addr common.Address, slot common.Hash, blockNumber *big.Int) w3types.RPCCallerFactory[common.Hash] {
-	return module.NewFactory[common.Hash](
+	return module.NewFactory(
 		"eth_getStorageAt",
 		[]any{addr, slot, module.BlockNumberArg(blockNumber)},
+		module.WithRetWrapper(func(ret *common.Hash) any { return (*hexutil.Hash)(ret) }),
 	)
 }
