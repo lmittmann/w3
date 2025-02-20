18ab97dba399f723e2dba1432f33f8fded65a636
lmittmann <lmittmann@users.noreply.github.com>
2025-02-20 21:14:48 +0100

    module/eth: added `Syncing()`

---
+++ module/eth/syncing.go
@@ -0,0 +1,29 @@
+package eth
+
+import (
+	"github.com/lmittmann/w3/internal/module"
+	"github.com/lmittmann/w3/w3types"
+)
+
+func Syncing() w3types.RPCCallerFactory[bool] {
+	return module.NewFactory(
+		"eth_syncing",
+		[]any{},
+		module.WithRetWrapper(syncingRetWrapper),
+	)
+}
+
+func syncingRetWrapper(ret *bool) any {
+	return (*syncingBool)(ret)
+}
+
+type syncingBool bool
+
+func (s *syncingBool) UnmarshalJSON(b []byte) error {
+	if string(b) == "false" {
+		*s = false
+	} else {
+		*s = true
+	}
+	return nil
+}
