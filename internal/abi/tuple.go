c84b555bbac32682264eb6d0585e6c8b05cc5003
lmittmann <lmittmann@users.noreply.github.com>
2025-06-12 21:11:20 +0200

    init typeOfField

--- internal/abi/tuple.go
+++ internal/abi/tuple.go
@@ -3,9 +3,12 @@ package abi
 import (
 	"errors"
 	"fmt"
+	"math/big"
 	"reflect"
+	"strings"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 var errDuplicateTuple = errors.New("duplicate tuple definition")
@@ -34,12 +37,17 @@ func buildTuples(tuples ...any) (map[string]abi.Argument, error) {
 			return nil, fmt.Errorf("expected struct, got %s", typ.Kind())
 		}
 
+		if _, ok := types[typ.Name()]; ok {
+			return nil, fmt.Errorf("%w: %s", errDuplicateTuple, typ.Name())
+		}
+
 		rawNames := make([]string, typ.NumField())
 		elems := make([]*abi.Type, typ.NumField())
 		for i := range typ.NumField() {
 			field := typ.Field(i)
-			rawNames[i] = field.Name
-			elem, err := typeOf(field)
+			// Use lowercase field name to match ABI convention
+			rawNames[i] = strings.ToLower(field.Name)
+			elem, err := typeOfField(field)
 			if err != nil {
 				return nil, err
 			}
@@ -61,30 +69,111 @@ func buildTuples(tuples ...any) (map[string]abi.Argument, error) {
 	return types, nil
 }
 
-// typeOf returns the abi.Type of a struct field.
-func typeOf(field reflect.StructField) (*abi.Type, error) {
+// typeOfField returns the [abi.Type] of a struct field.
+func typeOfField(field reflect.StructField) (*abi.Type, error) {
 	const tagKey = "abitype"
 
-	tag, tagOk := field.Tag.Lookup(tagKey)
-	abiType, abiTypeOk := types[tag]
-	if tagOk {
-		if !abiTypeOk {
-			return nil, fmt.Errorf("unknown abi type %q for field %q", tag, field.Name)
+	tag, _ := field.Tag.Lookup(tagKey)
+	return typeOf(field.Type, tag) // tag is "" if not set
+}
+
+func typeOf(typ reflect.Type, abiType string) (*abi.Type, error) {
+	abiT, isBasicT := basicTypes[typ]
+	if !isBasicT {
+		if typ.Kind() == reflect.Slice {
+			elemAibT, err := typeOf(typ.Elem(), abiType)
+			if err != nil {
+				return nil, err
+			}
+			return &abi.Type{
+				T:    abi.SliceTy,
+				Elem: elemAibT,
+			}, nil
 		}
-		if abiTypeOk && abiType.GetType() != field.Type {
-			return nil, fmt.Errorf("abi type %q for field %q is not compatible with its type %q", tag, field.Name, field.Type)
+		if typ.Kind() == reflect.Array {
+			elemAibT, err := typeOf(typ.Elem(), abiType)
+			if err != nil {
+				return nil, err
+			}
+			return &abi.Type{
+				T:    abi.ArrayTy,
+				Elem: elemAibT,
+				Size: typ.Len(),
+			}, nil
 		}
-		return &abiType, nil
+		if typ.Kind() == reflect.Struct {
+			// todo
+		}
+		return nil, fmt.Errorf("unknown type %q", typ)
 	}
 
-	switch field.Type.Kind() {
-	case reflect.Bool:
-		return &abi.Type{T: abi.BoolTy}, nil
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return &abi.Type{T: abi.UintTy}, nil
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return &abi.Type{T: abi.IntTy}, nil
+	if abiType == "" {
+		// if no abiType is specified, return the basic type directly.
+		return &abiT, nil
 	}
 
-	return nil, nil
+	abiT, ok := types[abiType]
+	if !ok {
+		return nil, fmt.Errorf("unknown abi type %q", abiType)
+	}
+	if abiT.GetType() != typ && !(typ == reflect.TypeFor[*big.Int]() &&
+		(abiT.GetType().Kind() == reflect.Int16 ||
+			abiT.GetType().Kind() == reflect.Int32 ||
+			abiT.GetType().Kind() == reflect.Int64 ||
+			abiT.GetType().Kind() == reflect.Uint16 ||
+			abiT.GetType().Kind() == reflect.Uint32 ||
+			abiT.GetType().Kind() == reflect.Uint64)) {
+		return nil, fmt.Errorf("tagged type %q does not match type %v", abiType, typ)
+	}
+	return &abiT, nil
+}
+
+var basicTypes = map[reflect.Type]abi.Type{
+	reflect.TypeFor[bool]():           {T: abi.BoolTy},
+	reflect.TypeFor[byte]():           {T: abi.UintTy, Size: 8},
+	reflect.TypeFor[uint8]():          {T: abi.UintTy, Size: 8},
+	reflect.TypeFor[uint16]():         {T: abi.UintTy, Size: 16},
+	reflect.TypeFor[uint32]():         {T: abi.UintTy, Size: 32},
+	reflect.TypeFor[uint64]():         {T: abi.UintTy, Size: 64},
+	reflect.TypeFor[int8]():           {T: abi.IntTy, Size: 8},
+	reflect.TypeFor[int16]():          {T: abi.IntTy, Size: 16},
+	reflect.TypeFor[int32]():          {T: abi.IntTy, Size: 32},
+	reflect.TypeFor[int64]():          {T: abi.IntTy, Size: 64},
+	reflect.TypeFor[[1]byte]():        {T: abi.FixedBytesTy, Size: 1},
+	reflect.TypeFor[[2]byte]():        {T: abi.FixedBytesTy, Size: 2},
+	reflect.TypeFor[[3]byte]():        {T: abi.FixedBytesTy, Size: 3},
+	reflect.TypeFor[[4]byte]():        {T: abi.FixedBytesTy, Size: 4},
+	reflect.TypeFor[[5]byte]():        {T: abi.FixedBytesTy, Size: 5},
+	reflect.TypeFor[[6]byte]():        {T: abi.FixedBytesTy, Size: 6},
+	reflect.TypeFor[[7]byte]():        {T: abi.FixedBytesTy, Size: 7},
+	reflect.TypeFor[[8]byte]():        {T: abi.FixedBytesTy, Size: 8},
+	reflect.TypeFor[[9]byte]():        {T: abi.FixedBytesTy, Size: 9},
+	reflect.TypeFor[[10]byte]():       {T: abi.FixedBytesTy, Size: 10},
+	reflect.TypeFor[[11]byte]():       {T: abi.FixedBytesTy, Size: 11},
+	reflect.TypeFor[[12]byte]():       {T: abi.FixedBytesTy, Size: 12},
+	reflect.TypeFor[[13]byte]():       {T: abi.FixedBytesTy, Size: 13},
+	reflect.TypeFor[[14]byte]():       {T: abi.FixedBytesTy, Size: 14},
+	reflect.TypeFor[[15]byte]():       {T: abi.FixedBytesTy, Size: 15},
+	reflect.TypeFor[[16]byte]():       {T: abi.FixedBytesTy, Size: 16},
+	reflect.TypeFor[[17]byte]():       {T: abi.FixedBytesTy, Size: 17},
+	reflect.TypeFor[[18]byte]():       {T: abi.FixedBytesTy, Size: 18},
+	reflect.TypeFor[[19]byte]():       {T: abi.FixedBytesTy, Size: 19},
+	reflect.TypeFor[[20]byte]():       {T: abi.FixedBytesTy, Size: 20},
+	reflect.TypeFor[[21]byte]():       {T: abi.FixedBytesTy, Size: 21},
+	reflect.TypeFor[[22]byte]():       {T: abi.FixedBytesTy, Size: 22},
+	reflect.TypeFor[[23]byte]():       {T: abi.FixedBytesTy, Size: 23},
+	reflect.TypeFor[[24]byte]():       {T: abi.FixedBytesTy, Size: 24},
+	reflect.TypeFor[[25]byte]():       {T: abi.FixedBytesTy, Size: 25},
+	reflect.TypeFor[[26]byte]():       {T: abi.FixedBytesTy, Size: 26},
+	reflect.TypeFor[[27]byte]():       {T: abi.FixedBytesTy, Size: 27},
+	reflect.TypeFor[[28]byte]():       {T: abi.FixedBytesTy, Size: 28},
+	reflect.TypeFor[[29]byte]():       {T: abi.FixedBytesTy, Size: 29},
+	reflect.TypeFor[[30]byte]():       {T: abi.FixedBytesTy, Size: 30},
+	reflect.TypeFor[[31]byte]():       {T: abi.FixedBytesTy, Size: 31},
+	reflect.TypeFor[[32]byte]():       {T: abi.FixedBytesTy, Size: 32},
+	reflect.TypeFor[common.Address](): {T: abi.AddressTy, Size: 20},
+	reflect.TypeFor[common.Hash]():    {T: abi.FixedBytesTy, Size: 32},
+	reflect.TypeFor[string]():         {T: abi.StringTy},
+	reflect.TypeFor[[]byte]():         {T: abi.BytesTy},
+	reflect.TypeFor[*big.Int]():       {T: abi.UintTy, Size: 256},
 }
--- internal/abi/tuple_test.go
+++ internal/abi/tuple_test.go
@@ -10,7 +10,10 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+	"github.com/lmittmann/w3/internal"
 )
 
 func TestTupleMap(t *testing.T) {
@@ -108,15 +111,148 @@ type Tuple2 struct {
 	Arg1 *big.Int `abitype:"int256"`
 }
 
-func TestConvert(t *testing.T) {
-	valSrc := struct {
-		Arg0 *big.Int
-		Arg1 *big.Int
+func TestTypeOfField(t *testing.T) {
+	tests := []struct {
+		Field   reflect.StructField
+		Want    *abi.Type
+		WantErr error
 	}{
-		Arg0: big.NewInt(123),
-		Arg1: big.NewInt(456),
+		// default types
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[bool]()},
+			Want:  &abi.Type{T: abi.BoolTy},
+		},
+		{
+			Field: reflect.StructField{Name: "TestUint8", Type: reflect.TypeFor[uint8]()},
+			Want:  &abi.Type{T: abi.UintTy, Size: 8},
+		},
+		{
+			Field: reflect.StructField{Name: "TestUint16", Type: reflect.TypeFor[uint16]()},
+			Want:  &abi.Type{T: abi.UintTy, Size: 16},
+		},
+		{
+			Field: reflect.StructField{Name: "TestUint32", Type: reflect.TypeFor[uint32]()},
+			Want:  &abi.Type{T: abi.UintTy, Size: 32},
+		},
+		{
+			Field: reflect.StructField{Name: "TestUint64", Type: reflect.TypeFor[uint64]()},
+			Want:  &abi.Type{T: abi.UintTy, Size: 64},
+		},
+		{
+			Field: reflect.StructField{Name: "TestInt8", Type: reflect.TypeFor[int8]()},
+			Want:  &abi.Type{T: abi.IntTy, Size: 8},
+		},
+		{
+			Field: reflect.StructField{Name: "TestInt16", Type: reflect.TypeFor[int16]()},
+			Want:  &abi.Type{T: abi.IntTy, Size: 16},
+		},
+		{
+			Field: reflect.StructField{Name: "TestInt32", Type: reflect.TypeFor[int32]()},
+			Want:  &abi.Type{T: abi.IntTy, Size: 32},
+		},
+		{
+			Field: reflect.StructField{Name: "TestInt64", Type: reflect.TypeFor[int64]()},
+			Want:  &abi.Type{T: abi.IntTy, Size: 64},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBigInt", Type: reflect.TypeFor[*big.Int]()},
+			Want:  &abi.Type{T: abi.UintTy, Size: 256},
+		},
+		{
+			Field: reflect.StructField{Name: "TestAddress", Type: reflect.TypeFor[common.Address]()},
+			Want:  &abi.Type{T: abi.AddressTy, Size: 20},
+		},
+		{
+			Field: reflect.StructField{Name: "TestHash", Type: reflect.TypeFor[common.Hash]()},
+			Want:  &abi.Type{T: abi.FixedBytesTy, Size: 32},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBytes", Type: reflect.TypeFor[[]byte]()},
+			Want:  &abi.Type{T: abi.BytesTy},
+		},
+		{
+			Field: reflect.StructField{Name: "TestString", Type: reflect.TypeFor[string]()},
+			Want:  &abi.Type{T: abi.StringTy},
+		},
+
+		// slice types
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[]bool]()},
+			Want:  &abi.Type{T: abi.SliceTy, Elem: &abi.Type{T: abi.BoolTy}},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[1]bool]()},
+			Want:  &abi.Type{T: abi.ArrayTy, Elem: &abi.Type{T: abi.BoolTy}, Size: 1},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[][]bool]()},
+			Want:  &abi.Type{T: abi.SliceTy, Elem: &abi.Type{T: abi.SliceTy, Elem: &abi.Type{T: abi.BoolTy}}},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[1][]bool]()},
+			Want:  &abi.Type{T: abi.ArrayTy, Elem: &abi.Type{T: abi.SliceTy, Elem: &abi.Type{T: abi.BoolTy}}, Size: 1},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[][1]bool]()},
+			Want:  &abi.Type{T: abi.SliceTy, Elem: &abi.Type{T: abi.ArrayTy, Elem: &abi.Type{T: abi.BoolTy}, Size: 1}},
+		},
+		{
+			Field: reflect.StructField{Name: "TestBool", Type: reflect.TypeFor[[1][1]bool]()},
+			Want:  &abi.Type{T: abi.ArrayTy, Elem: &abi.Type{T: abi.ArrayTy, Elem: &abi.Type{T: abi.BoolTy}, Size: 1}, Size: 1},
+		},
+
+		// tagged types
+		{
+			Field: reflect.StructField{Name: "TestTagUint256", Type: reflect.TypeFor[*big.Int](), Tag: `abitype:"uint256"`},
+			Want:  &abi.Type{T: abi.UintTy, Size: 256},
+		},
+		{
+			Field: reflect.StructField{Name: "TestTagUint256", Type: reflect.TypeFor[*big.Int](), Tag: `abitype:"uint16"`},
+			Want:  &abi.Type{T: abi.UintTy, Size: 16},
+		},
+		{
+			Field: reflect.StructField{Name: "TestTagInt256", Type: reflect.TypeFor[*big.Int](), Tag: `abitype:"int256"`},
+			Want:  &abi.Type{T: abi.IntTy, Size: 256},
+		},
+		{
+			Field: reflect.StructField{Name: "TestTagAddress", Type: reflect.TypeFor[common.Address](), Tag: `abitype:"address"`},
+			Want:  &abi.Type{T: abi.AddressTy, Size: 20},
+		},
+		{
+			Field: reflect.StructField{Name: "TestTagBytes32", Type: reflect.TypeFor[[32]byte](), Tag: `abitype:"bytes32"`},
+			Want:  &abi.Type{T: abi.FixedBytesTy, Size: 32},
+		},
+		{
+			Field:   reflect.StructField{Name: "TestTagBytes32Hash", Type: reflect.TypeFor[common.Hash](), Tag: `abitype:"bytes32"`},
+			WantErr: errors.New(`tagged type "bytes32" does not match type common.Hash`),
+		},
+		{
+			Field:   reflect.StructField{Name: "TestUnknownTag", Type: reflect.TypeFor[*big.Int](), Tag: `abitype:"unknown"`},
+			WantErr: errors.New(`unknown abi type "unknown"`),
+		},
+		{
+			Field:   reflect.StructField{Name: "TestIncompatible", Type: reflect.TypeFor[string](), Tag: `abitype:"uint256"`},
+			WantErr: errors.New(`tagged type "uint256" does not match type string`),
+		},
+		{
+			Field:   reflect.StructField{Name: "TestUnsupported", Type: reflect.TypeFor[float64]()},
+			WantErr: errors.New(`unknown type "float64"`),
+		},
 	}
 
-	valDst := abi.ConvertType(valSrc, Tuple2{})
-	t.Logf("valDst: %+v", valDst)
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			got, err := typeOfField(test.Field)
+			if diff := cmp.Diff(test.WantErr, err,
+				internal.EquateErrors(),
+			); diff != "" {
+				t.Fatalf("Err: (-want +got):\n%s", diff)
+			}
+			if diff := cmp.Diff(test.Want, got,
+				cmpopts.IgnoreUnexported(abi.Type{}),
+			); diff != "" {
+				t.Errorf("(-want +got):\n%s", diff)
+			}
+		})
+	}
 }
